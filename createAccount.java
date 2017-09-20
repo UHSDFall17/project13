2381173df3cbe29c6f0044faf7998f6e3d718ddb
skgiang <skgiang@uh.edu>
2017-09-19 21:32:39 -0500

    createAccount.java
    createAccount version1: Welcome

---
+++ createAccount.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class createAccount {
+    private String name, email, secQ_1,secQ_2,secA_1,secA_2;
+    private char[] pswd;
+    
+    /*******************************
+    *           WELCOME            *
+    *******************************/ 
+    protected void welcome() {
+        System.out.println("Welcome!\nCREATE AN ACCOUNT");
+        //System.out.println("If you find out that you are in the wrong place, enter "Log in" at anytime.");
+    }
+}
