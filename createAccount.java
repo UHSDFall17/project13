380758b6585217dc1ef4d0d45f752e389bed36de
skgiang <skgiang@uh.edu>
2017-09-19 21:46:34 -0500

    version5: setPswd, goodPwsd
    setPswd: allows user to create a password for their new account
    goodPswd: checks quality of password including length and character requirements

--- createAccount.java
+++ createAccount.java
@@ -5,17 +5,17 @@ public class createAccount {
     private String name, email, secQ_1,secQ_2,secA_1,secA_2;
     private char[] pswd;
     
-    /*******************************
-    *           WELCOME            *
-    *******************************/ 
+/*******************************
+*           WELCOME            *
+*******************************/ 
     protected void welcome() {
         System.out.println("Welcome!\nCREATE AN ACCOUNT");
         //System.out.println("If you find out that you are in the wrong place, enter "Log in" at anytime.");
     }
     
-    /*******************************
-    *           NAME               *
-    *******************************/    
+/*******************************
+*           NAME               *
+*******************************/    
     protected void setName(Console console) {
         System.out.println("Name: ");
         name = (console.readLine()).toUpperCase();
@@ -31,9 +31,9 @@ public class createAccount {
       return true;
     }
     
-    /*******************************
-    *           EMAIL              *
-    *******************************/
+/*******************************
+*           EMAIL              *
+*******************************/
     protected void setEmail(Console console) {
         System.out.println("Email: ");
         email = (console.readLine()).toLowerCase();
@@ -94,5 +94,41 @@ public class createAccount {
         if(new File("Accounts/" + email + ".txt").exists())
           return true;
         return false;          
+    }
+    
+/*******************************
+*           PASSWORD           *
+*******************************/
+    
+    protected void setPswd(Console console) {
+        System.out.print("\nPassword Requirements: \n \t(1) 6-20 characters \n \t(2) at least 1 upper-case letter \n \t(3) at least 1 digit \n \t(4) at least 1 special character\nNOTICE: Input will not be displayed but will still be recognized.\n");
+        System.out.println("Password: ");
+        pswd = console.readPassword();
+        
+        if(!goodPswd()) { //input does NOT meet all criteria
+            System.out.print("Invalid Password.\n\n");
+            /** CLEAR SCREEN **/
+            setPswd(console); 
+        }
+        confirmPswd(console);
+    }
+    
+    protected boolean goodPswd(){
+        if(pswd.length >= 6 && pswd.length <= 20) //Criterion: MIN 6, MAX 20 characters
+        {
+            int countUpper = 0;    int countDigit = 0;   int countSpecial = 0;
+            for(char c : pswd){
+                if(c >= 65 && c <= 90) {countUpper++;} //Upper-case letter
+                else if(c >= 48 && c <= 57) {countDigit++;} //Digit
+                else if(c >= 97 && c <= 122) {} //lowercase letter, do nothing
+                else{countSpecial++;} //Special character
+            }
+            
+            //MIN 1 special character, MIN 1 uppercase, MIN 1 digit
+            if(countUpper > 0 && countDigit > 0 && countSpecial > 0)
+              return true;
+            return false; //missing upper, digit, or special character
+        }
+        return false; //too short or too long
     }
 }
