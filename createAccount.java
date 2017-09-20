4f52332149033ff6c13d95501663bf5d6632abf4
skgiang <skgiang@uh.edu>
2017-09-19 21:40:59 -0500

    version3: setEmail, confirmEmail
    Also included: Welcome, name
    Added "protected" to name's function type
    Added setEmail and confirmEmail functions to receive and confirm user's entry

--- createAccount.java
+++ createAccount.java
@@ -16,7 +16,7 @@ public class createAccount {
     /*******************************
     *           NAME               *
     *******************************/    
-    void setName(Console console) {
+    protected void setName(Console console) {
         System.out.println("Name: ");
         name = (console.readLine()).toUpperCase();
         if(!goodName(name)) {
@@ -25,10 +25,46 @@ public class createAccount {
         }
     }
     
-    boolean goodName(String name){
+    protected boolean goodName(String name){
       if(name.equals(""))
         return false;
       return true;
     }
     
+    /*******************************
+    *           EMAIL              *
+    *******************************/
+    protected void setEmail(Console console) {
+        System.out.println("Email: ");
+        email = (console.readLine()).toLowerCase();
+        
+        /** CONFIRM **/
+        confirmEmail(email, console);
+        
+
+        if(!goodEmail(email)){ //checks for '@' and valid extension
+            System.out.println("\n\nError: Invalid Email Format.");
+            setEmail(console);
+        }
+        else if(rgsdEmail(email)) { //email had already been registered
+            System.out.println("\n\nError: Email Already Registered. Log in or try again.");
+            setEmail(console);
+        }
+        else {} //good, unregistered email
+    }
+    
+    protected void confirmEmail(String email, Console console){
+        System.out.println("Confirm Email: " + email + "(Y/N)");
+        String temp = (console.readLine()).toUpperCase();
+        if(temp.equals("")){
+            System.out.println("Please enter Y or N.");
+            confirmEmail(email, console);
+        } 
+        else if(temp.equals("N")){
+      		//CLEAR SCREEN
+      		System.out.println("Name: " + name + "\n");
+      		setEmail(console);
+      	}
+        else{}	
+    }
 }
