925fcf09eecd988bdb3472d58888afdf482c07bd
skgiang <skgiang@uh.edu>
2017-09-19 21:36:30 -0500

    createAccount version 2: Name
    Also included: Welcome

--- createAccount.java
+++ createAccount.java
@@ -12,4 +12,23 @@ public class createAccount {
         System.out.println("Welcome!\nCREATE AN ACCOUNT");
         //System.out.println("If you find out that you are in the wrong place, enter "Log in" at anytime.");
     }
+    
+    /*******************************
+    *           NAME               *
+    *******************************/    
+    void setName(Console console) {
+        System.out.println("Name: ");
+        name = (console.readLine()).toUpperCase();
+        if(!goodName(name)) {
+            System.out.println("Error: Please enter your name.\n");
+            setName(console);
+        }
+    }
+    
+    boolean goodName(String name){
+      if(name.equals(""))
+        return false;
+      return true;
+    }
+    
 }
