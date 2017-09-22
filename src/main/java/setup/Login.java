5a2f2a26fc5a45b7b5a5641ed6028c4d0fd38743
Tommy Huynh <Tom_T_Huynh@Outlook.com>
2017-09-22 12:49:58 -0500

    Fix login

--- src/main/java/setup/Login.java
+++ src/main/java/setup/Login.java
@@ -6,57 +6,53 @@ import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class Login
+public class Login 
 {
-    protected void login()
-    {
-        Console console = System.console();
-        String user = console.readLine("Username: ");
-        char[] pass = console.readPassword("Password: ");
-        String pwd = new String(pass);
-
-        if(authorization(user.toLowerCase(), pwd) == true)
-        {
+	protected void login()
+	{
+		Console console = System.console();
+		String user = console.readLine("Username: ");
+		char[] pass = console.readPassword("Password: ");
+		String pwd = new String(pass);
+		
+		if(authorization(user.toLowerCase(), pwd))
+		{
 			/*Access account.*/
-        }
-        else
-        {
-            System.out.println("Invalid username or password.");
-        }
-    }
-    private boolean authorization(String user, String pass)
-    {
-        String line = null;
-        String fileName = user + ".txt";
-        boolean autherized = false;
-        try
-        {
-            FileReader fileReader = new FileReader(fileName);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            if((line = bufferedReader.readLine()) != null)
-            {
-                String [] token = line.split(" ");
-                if(token[0].equals(pass))
-                {
-                    autherized = true;
-                    bufferedReader.close();
-                }
-                else
-                {
-                    autherized = false;
-                    System.out.println("Password is invalid.");
-                    bufferedReader.close();
-                }
-            }
-        }
-        catch(FileNotFoundException e)
-        {
-            System.out.println("File not found: '" + fileName + "'.");
-        }
-        catch(IOException e)
-        {
-            System.out.println("Error reading file: '" + fileName + "'.");
-        }
-        return autherized;
-    }
+		}
+	}
+	private boolean authorization(String user, String pass)
+	{
+		String line = null;
+		String fileName = "Accounts/" + user + "/loginInfo.txt";
+		boolean autherized = false;
+		try
+		{
+			FileReader fileReader = new FileReader(fileName);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			if((line = bufferedReader.readLine()) != null)
+			{
+				String [] token = line.split(" ");
+				if(token[0].equals(pass)) 
+				{
+					autherized = true;
+					bufferedReader.close();
+				}
+				else
+				{
+					autherized = false;
+					System.out.println("Password is invalid.");
+					bufferedReader.close();
+				}
+			}
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.println("User not found: '" + fileName + "'.");
+		}
+		catch(IOException e) 
+		{
+	        System.out.println("Error reading file: '" + fileName + "'.");                  
+	    }
+		return autherized;
+	}
 }
