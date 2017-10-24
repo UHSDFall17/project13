3d00f41d0024ca7307dd2fa964f98f43e39121df
skgiang <skgiang@uh.edu>
2017-10-24 15:23:38 -0500

    cleaned up: satisfies single responsibility

--- src/main/java/setup/Account.java
+++ src/main/java/setup/Account.java
@@ -3,7 +3,7 @@ package setup;
 import java.io.*;
 import java.util.*;
 
-public class Account{
+public class Account {
     Scanner scanner = new Scanner(System.in);
 
     private String name, email, pswd, ans1, ans2;
@@ -17,10 +17,11 @@ public class Account{
     CheckSecurityQA newSQ1;
     CheckSecurityQA newSQ2;
 
-    public Account(){}
+    public Account() {
+    }
 
-    public void createNewAccount(){
-        User newUser = new User();
+    public void createNewAccount() {
+        Name newUser = new Name();
         newUser.setNewName();
 
         Email newEmail = new Email();
@@ -39,115 +40,23 @@ public class Account{
         write.saveNewAccount(newEmail.email, newPassword.newPswd, newUser.name, newSQ1.sq1, newSQ1.ans1, newSQ2.sq2, newSQ2.ans2);
     }
 
-    public void resetForgottenPassword(){
-
+    public void resetForgottenPassword() {
+        Password password = new Password();
+        password.resetPassword();
     }
 
-    public void changeName(){}
-
-    public void changePassword(){}
-
-    public void changeSQ(){}
-
-
-    /*******************************
-     *           EMAIL              *
-     *******************************/
-    protected void setEmail() {
-        System.out.print("Email: ");
-        email = (scanner.nextLine()).toLowerCase();
-
-        newEmail = new CheckEmail();
-        if(!newEmail.goodEmail(email)){ //checks for '@' and valid extension
-            System.out.println("\nError: Invalid Email Format.");
-            setEmail();
-        }
-        else if(newEmail.isRegistered(email)) { //email had already been registered
-            System.out.println("\nError: Email Already Registered.\n Press ENTER to try again or '0' to log in.");
-            if((scanner.nextLine()).equals("0"))
-                System.exit(1);
-            setEmail();
-        }
-        else { //good, unregistered email
-            confirmEmail(email);
-        }
+    public void changeName() {
+        Name name = new Name();
+        name.changeName();
     }
 
-    protected void confirmEmail(String email){
-        System.out.println("Confirm Email: " + email + " (Y/N)");
-        String temp = (scanner.nextLine()).toUpperCase();
-        if(temp.equals("")){
-            System.out.println("Please enter Y or N.");
-            confirmEmail(email);
-        }
-        else if(temp.equals("N")){
-            System.out.println("Name: " + name);
-            setEmail();
-        }
-        else{}
+    public void changePassword() {
+        Password password = new Password();
+        password.changePassword();
     }
 
-    /*******************************
-     *           PASSWORD           *
-     *******************************/
-
-
-    /*******************************
-     *      SECURITY QUESTIONS      *
-     *******************************/
-
-    public void setSecurityQuestion1() { // Security Question and Answer #1
-        newSQ1 = new CheckSecurityQA();
-        System.out.println("\nSelect Security Question #1");
-        for (int i = 1; i <= 4; i++)
-            System.out.println("\t" + i + " - " + newSQ1.getQuestion(i));
-        testQ = scanner.nextLine().charAt(0);
-
-
-        if (!newSQ1.validSelection(1, testQ)) {
-            System.out.println("Input is invalid. Try again.");
-            setSecurityQuestion1();
-        }
-        else {
-            secQ_1 = Character.getNumericValue(testQ);
-            System.out.println("\nPress 1 to change Security Question #1.");
-            System.out.println("Question: " + newSQ1.getQuestion(secQ_1));
-            System.out.print("Answer: ");
-            ans1 = scanner.nextLine().toUpperCase();
-            if (ans1.equals("1")) {
-                setSecurityQuestion1();
-            }
-            else if(ans1.isEmpty()){
-                System.out.println("Enter an answer for Security Question #1.");
-            }
-            else{}
-        }
-    }
-
-    public void setSecurityQuestion2(){
-        newSQ2 = new CheckSecurityQA();
-        System.out.println("\nSelect Security Question #2");
-        for(int i = 5; i <= 8; i++)
-            System.out.println("\t" + i + " - " + newSQ2.getQuestion(i));
-       testQ = scanner.nextLine().charAt(0);
-
-        if (!newSQ2.validSelection(2, testQ)) {
-            System.out.println("Input is invalid. Try again.");
-            setSecurityQuestion2();
-        }
-        else {
-            secQ_2 = Character.getNumericValue(testQ);
-            System.out.println("\nPress 1 to change Security Question #2.");
-            System.out.println("Question: " + newSQ2.getQuestion(secQ_2));
-            System.out.print("Answer: ");
-            ans2 = scanner.nextLine().toUpperCase();
-            if (ans2.equals("1")) {
-                setSecurityQuestion2();
-            }
-            else if(ans2.isEmpty()){
-                System.out.println("Enter an answer for Security Question #2.");
-            }
-            else{}
-        }
+    public void changeSQ() {
+        SecurityQuestions securityQuestions = new SecurityQuestions();
+        securityQuestions.changeSQ();
     }
 }
