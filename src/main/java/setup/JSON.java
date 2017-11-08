06984f4621a01f725c913dcc56ed269a6dcc560f
skgiang <skgiang@uh.edu>
2017-11-07 19:36:23 -0600

    removed commented, unused lines

--- src/main/java/setup/JSON.java
+++ src/main/java/setup/JSON.java
@@ -1,12 +1,9 @@
 package setup;
 
 import app.*;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.annotation.JsonAppend;
-import com.fasterxml.jackson.databind.node.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,19 +14,10 @@ public class JSON {
 
     public JSON() {}
 
-//    public void practiceJNode(){
-//        JsonNodeFactory nodeFactory = new JsonNodeFactory();
-//        ObjectNode node = nodeFactory.objectNode();
-//    }
-
-
     public void createDefaultJson(String userEmail){
         mapper = new ObjectMapper();
 
         Dashboard dashboard = new Dashboard();
-
-
-        //Object o = dashboard.getLists().toArray();
 
         try {
             mapper.writerWithDefaultPrettyPrinter().writeValue(new File("Accounts/" + userEmail + "/data.json"), dashboard);
@@ -41,33 +29,5 @@ public class JSON {
             e.printStackTrace();
         }
     }
-//
-//    public void addTasktoJson(String userEmail, ArrayList<Tasks> taskList){
-//        mapper = new ObjectMapper();
-//
-////        Dashboard dashboard = new Dashboard(userEmail);
-////        //String listName = list.getListName();
-////        List list = new List("Personal");
-//
-//        /*Tasks tasks = new Tasks();
-//        //list.createTask(tasks, "Call mom");
-//        //tasks.getListName();
-//        tasks.getDescription();
-//        //tasks.getSubTasks();
-//        tasks.getNotification();
-//        tasks.getTimestamp();
-//        //tasks.getPrioritized();
-//        //tasks.getNotes();*/
-//
-//        try {
-//            mapper.writerWithDefaultPrettyPrinter().writeValue(new File("Accounts/" + userEmail + "/data.json"), taskList);
-//        } catch (JsonGenerationException e) {
-//            e.printStackTrace();
-//        } catch (JsonMappingException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
 
 }
