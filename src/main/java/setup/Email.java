a9a25521e2a8ceb93dfae2c48bc6205ffcd97af3
skgiang <skgiang@uh.edu>
2017-11-19 03:09:15 -0600

    added confirm email step updated method type to return info

--- src/main/java/setup/Email.java
+++ src/main/java/setup/Email.java
@@ -3,7 +3,7 @@ package setup;
 import java.util.Scanner;
 
 public class Email {
-    private String inputEmail;
+    private String inputEmail = "";
 
     Scanner input = new Scanner(System.in);
     CheckEmail checkEmail;
@@ -12,17 +12,24 @@ public class Email {
 
     public Email(){}
 
-    public void setNewEmail(){
-        System.out.print("Email: ");
-        inputEmail = input.nextLine().toLowerCase();
+    public String setAndGetNewEmail(){
+        String emailConfirmer = "not confirmed";
+        while(!emailConfirmer.equals(inputEmail)) {
+            System.out.print("Email: ");
+            inputEmail = input.nextLine().toLowerCase();
+
+            /* CONFIRM EMAIL */
+            System.out.print("Confirm Email: ");
+            emailConfirmer = input.nextLine().toLowerCase();
+        }
 
         checkEmail = new CheckEmail();
         if(!checkEmail.goodEmail(inputEmail)|| checkEmail.isRegistered(inputEmail)){
             System.out.println("Invalid: Email entered is already registered or does not follow standard formatting rules.");
-            setNewEmail();
+            return setAndGetNewEmail();
         }
         else{
-            email = inputEmail;
+           return inputEmail;
         }
     }
 
