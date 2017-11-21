daac84c7dccdf61c9798b8878a71469ac2fbd635
Ramon <ramirez.roberto45@gmail.com>
2017-11-21 14:21:39 -0600

    created user class

---
+++ src/main/java/setup/User.java
@@ -0,0 +1,43 @@
+package setup;
+
+public class User
+{
+    private String username, password, name, answer1, answer2;
+    private int question1, question2;
+
+    public User(){}
+
+    public User(String un, String pw, String name, String q1, String ans1, String q2, String ans2)
+    {
+        username = un; password = pw; this.name = name; answer1 = ans1; answer2 = ans2;
+        question1 = Integer.parseInt(q1); question2 = Integer.parseInt(q2);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getQuestion1() {
+        return question1;
+    }
+
+    public int getQuestion2() {
+        return question2;
+    }
+
+    public String getAnswer1() {
+        return answer1;
+    }
+
+    public String getAnswer2() {
+        return answer2;
+    }
+}
