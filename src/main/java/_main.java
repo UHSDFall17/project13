897a8ccb41c737eba243bf3c07963517addb0604
skgiang <skgiang@uh.edu>
2017-11-27 23:04:40 -0600

    clean main

--- src/main/java/_main.java
+++ src/main/java/_main.java
@@ -1,32 +1,13 @@
 import app.*;
 import setup.*;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 
 public class _main
 {
-    public static void main(String args[])
-    {
-        //Account account = new Account();
-        //account.createNewAccount();
-        //account.logIn();
-//        account.resetForgottenPassword();
-//        account.changeName();
-//        account.changePassword();
-//        account.changeSQ();;
-
-//        JSON obj = new JSON();
-//        obj.createDefaultJson("johnsmith@gmail.com");
-
-        //obj.addTasktoJson("johnsmith@gmail.com");
-
-        //ResetPswd recoverForgotten = new ResetPswd();
-        //Dashboard test = new Dashboard();
-        //test.commandHandler();
-//        Login test = new Login();
-//        test.access();
+    public static void main(String args[]) {
         AnyDo test = new AnyDo();
-
-
-
     }
 }
