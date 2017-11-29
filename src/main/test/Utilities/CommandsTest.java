24edebc43e7788136d385389f8bf4c8a717c7705
skgiang <skgiang@uh.edu>
2017-11-29 00:50:37 -0600

    testCommandsHashMapPut

--- src/main/test/Utilities/CommandsTest.java
+++ src/main/test/Utilities/CommandsTest.java
@@ -2,6 +2,9 @@ package Utilities;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class CommandsTest {
@@ -54,5 +57,27 @@ public class CommandsTest {
         String[] testCommandsOffered = {"Command A", "Command B", "Command C"};
         commands = new Commands(testCommandsOffered);
         assertNotEquals("List of commands available:\n1 - Command A\n2 - Command B\n3 - Command C", commands.toString()); //missing \n (end line)
+    }
+
+    @Test
+    public void testCommandsHashMapPut_Correct(){
+        String[] testCommandsOffered = {"Command A", "Command B", "Command C"};
+        commands = new Commands(testCommandsOffered);
+        Map<Integer, String> hashMap = new HashMap<Integer, String>();
+        hashMap.put(1, "Command A");
+        hashMap.put(2, "Command B");
+        hashMap.put(3, "Command C");
+        assertEquals(commands.getCommands(), hashMap);
+    }
+
+    @Test
+    public void testCommandsHashMapPut_Wrong(){
+        String[] testCommandsOffered = {"Command A", "Command B", "Command C", "Command D"};
+        commands = new Commands(testCommandsOffered);
+        Map<Integer, String> hashMap = new HashMap<Integer, String>();
+        hashMap.put(1, "Command A");
+        hashMap.put(2, "Command B");
+        hashMap.put(3, "Command C");
+        assertNotEquals(commands.getCommands(), hashMap); //commands has 4 commands, hashmap has 3
     }
 }
