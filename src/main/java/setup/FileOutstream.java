653d6d845e919ea698d2c1a6cbed3cd98a872590
skgiang <skgiang@uh.edu>
2017-11-29 03:36:21 -0600

    throw instead of try-catch blocks -- purpose of testing

--- src/main/java/setup/FileOutstream.java
+++ src/main/java/setup/FileOutstream.java
@@ -10,58 +10,47 @@ public class FileOutstream {
 
     public FileOutstream(){ stream = new Stream();}
 
-    public void saveNewAccount(String email, String pswd, String name, String corporate, String[] QA1, String[] QA2) {
-        try {
-            File key = new File(System.getProperty("user.dir") + "/Accounts/" + email);
-            key.mkdirs();
+    public void saveNewAccount(String email, String pswd, String name, String corporate, String[] QA1, String[] QA2)
+            throws IOException {
+        File key = new File(System.getProperty("user.dir") + "/Accounts/" + email);
+        key.mkdirs();
 
-            PrintWriter account = new PrintWriter(System.getProperty("user.dir") + "/Accounts/" + email + "/accountInfo.txt");
+        PrintWriter account = new PrintWriter(System.getProperty("user.dir") + "/Accounts/" + email + "/accountInfo.txt");
 
-            account.println(pswd);
-            account.println(name);
+        account.println(pswd);
+        account.println(name);
 
-            account.println(corporate); //1 for corporate user -- 0 for non-corporate user
+        account.println(corporate); //1 for corporate user -- 0 for non-corporate user
 
-            account.println(QA1[0]);
-            account.println(QA1[1]);
+        account.println(QA1[0]);
+        account.println(QA1[1]);
 
-            account.println(QA2[0]);
-            account.print(QA2[1]);
+        account.println(QA2[0]);
+        account.print(QA2[1]);
 
-            account.close();
-        }
-        catch(IOException e) {
-            stream.writeToConsole("Something went wrong! Please try again.");
-            System.exit(1);
-        }
-        //return to LOGIN PAGE
+        account.close();
     }
 
-    public void updateName(String email, String oldName, String newName){
-        try {
-            BufferedReader inStream = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/Accounts/" + email + "/accountInfo.txt"));
-            StringBuffer getLine = new StringBuffer();
-            String oldLine;
+    public void updateName(String email, String oldName, String newName) throws IOException {
+        BufferedReader inStream = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/Accounts/" + email + "/accountInfo.txt"));
+        StringBuffer getLine = new StringBuffer();
+        String oldLine;
 
-            /* GRAB LINES AND OLD NAME*/
-            while((oldLine = inStream.readLine()) != null){
-                getLine.append(oldLine);
-                getLine.append("\n");
-            }
-            String line = getLine.toString();
-            inStream.close();
-
-            /* REPLACE OLD PSWD WITH NEW PSWD */
-            line = line.replace(oldName, newName);
-            FileOutputStream outStream = new FileOutputStream(System.getProperty("user.dir") + "/Accounts/" + email + "/accountInfo.txt");
-            outStream.write(line.getBytes());
-            outStream.close();
-
-            stream.writeToConsole("\nSUCCESS! HELLO, " + newName + ".");//SUCCESS
-        } catch (IOException e) {
-            e.printStackTrace();
-            stream.writeToConsole("\nFAILED: Your name has not been updated. Program Terminated.");
+        /* GRAB LINES AND OLD NAME*/
+        while((oldLine = inStream.readLine()) != null){
+            getLine.append(oldLine);
+            getLine.append("\n");
         }
+        String line = getLine.toString();
+        inStream.close();
+
+        /* REPLACE OLD PSWD WITH NEW PSWD */
+        line = line.replace(oldName, newName);
+        FileOutputStream outStream = new FileOutputStream(System.getProperty("user.dir") + "/Accounts/" + email + "/accountInfo.txt");
+        outStream.write(line.getBytes());
+        outStream.close();
+
+        stream.writeToConsole("\nSUCCESS! HELLO, " + newName + ".");//SUCCESS
     }
 
     public void updatePswd(String email, String oldPswd, String newPswd){
