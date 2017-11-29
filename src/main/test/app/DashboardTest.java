93e6f853cc14a6b75f94180c00df7cdd8777f57b
skgiang <skgiang@uh.edu>
2017-11-29 17:43:36 -0600

    testCommandCenter

--- src/main/test/app/DashboardTest.java
+++ src/main/test/app/DashboardTest.java
@@ -1,8 +1,11 @@
 package app;
 
 import static org.junit.Assert.*;
+
+import Utilities.Stream;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class DashboardTest
@@ -12,6 +15,36 @@ public class DashboardTest
     @Test
     public void Canary(){ //verifies good env
         assertTrue(true);
+    }
+
+    @Test
+    public void testCommandCenter_Failure() throws IOException {
+        test = new Dashboard();
+        assertEquals(0, test.commandCenter(0, "Command A\nCommand B\n"));
+    }
+
+    @Test
+    public void testCommandCenter_Case1_Success() throws IOException {
+        test = new Dashboard();
+        assertEquals(0, test.commandCenter(1, "Command A\nCommand B\n"));
+    }
+
+    @Test
+    public void testCommandCenter_Case7_Success() throws IOException {
+        test = new Dashboard();
+        assertEquals(2, test.commandCenter(7, "Command A\nCommand B\n"));
+    }
+
+    @Test
+    public void testCommandCenter_Case8_Success() throws IOException {
+        test = new Dashboard();
+        assertEquals(0, test.commandCenter(8, "Command A\nCommand B\n"));
+    }
+
+    @Test
+    public void testCommandCenter_Case9_Success() throws IOException {
+        test = new Dashboard();
+        assertEquals(1, test.commandCenter(9, "Command A\nCommand B\n"));
     }
 
     @Test
