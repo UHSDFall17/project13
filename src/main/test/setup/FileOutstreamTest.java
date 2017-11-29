a664ca70ffd28bff39f2679c129f34c745045884
skgiang <skgiang@uh.edu>
2017-11-29 02:52:52 -0600

    testUpdateName_SUCCESS()

--- src/main/test/setup/FileOutstreamTest.java
+++ src/main/test/setup/FileOutstreamTest.java
@@ -10,38 +10,38 @@ public class FileOutstreamTest {
     private FileOutstream fileOutstream;
 
     @Test
-    public void Canary(){ //verifies good env
+    public void Canary() { //verifies good env
         assertTrue(true);
     }
 
     @Test
-    public void testSaveNewAccount_FolderCreated_Success(){
+    public void testSaveNewAccount_FolderCreated_Success() {
         fileOutstream = new FileOutstream();
         String[] sqa1 = {"1", "MOM"};
         String[] sqa2 = {"5", "HTX"};
-        fileOutstream.saveNewAccount("testEmail@hotmail.com", "testPswd123+","testName","0",sqa1,sqa2);
+        fileOutstream.saveNewAccount("testEmail@hotmail.com", "testPswd123+", "testName", "0", sqa1, sqa2);
 
         File file = new File("Accounts/testEmail@hotmail.com");
         assertTrue(file.exists());
     }
 
     @Test
-    public void testSaveNewAccount_FileCreated_Success(){
+    public void testSaveNewAccount_FileCreated_Success() {
         fileOutstream = new FileOutstream();
         String[] sqa1 = {"1", "MOM"};
         String[] sqa2 = {"5", "HTX"};
-        fileOutstream.saveNewAccount("testEmail@hotmail.com", "testPswd123+","testName","0",sqa1,sqa2);
+        fileOutstream.saveNewAccount("testEmail@hotmail.com", "testPswd123+", "testName", "0", sqa1, sqa2);
 
         File file = new File("Accounts/testEmail@hotmail.com/accountInfo.txt");
         assertTrue(file.exists());
     }
 
     @Test
-    public void testSaveNewAccount_Password_Correct(){
+    public void testSaveNewAccount_Password_Correct() {
         fileOutstream = new FileOutstream();
         String[] sqa1 = {"1", "MOM"};
         String[] sqa2 = {"5", "HTX"};
-        fileOutstream.saveNewAccount("testEmail@hotmail.com", "testPswd123+","testName","0",sqa1,sqa2);
+        fileOutstream.saveNewAccount("testEmail@hotmail.com", "testPswd123+", "testName", "0", sqa1, sqa2);
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader("Accounts/testEmail@hotmail.com/accountInfo.txt"));
@@ -53,11 +53,11 @@ public class FileOutstreamTest {
     }
 
     @Test
-    public void testSaveNewAccount_Name_Correct(){
+    public void testSaveNewAccount_Name_Correct() {
         fileOutstream = new FileOutstream();
         String[] sqa1 = {"1", "MOM"};
         String[] sqa2 = {"5", "HTX"};
-        fileOutstream.saveNewAccount("testEmail@hotmail.com", "testPswd123+","testName","0",sqa1,sqa2);
+        fileOutstream.saveNewAccount("testEmail@hotmail.com", "testPswd123+", "testName", "0", sqa1, sqa2);
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader("Accounts/testEmail@hotmail.com/accountInfo.txt"));
@@ -70,11 +70,11 @@ public class FileOutstreamTest {
     }
 
     @Test
-    public void testSaveNewAccount_Corporate_Correct(){
+    public void testSaveNewAccount_Corporate_Correct() {
         fileOutstream = new FileOutstream();
         String[] sqa1 = {"1", "MOM"};
         String[] sqa2 = {"5", "HTX"};
-        fileOutstream.saveNewAccount("testEmail@hotmail.com", "testPswd123+","testName","0",sqa1,sqa2);
+        fileOutstream.saveNewAccount("testEmail@hotmail.com", "testPswd123+", "testName", "0", sqa1, sqa2);
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader("Accounts/testEmail@hotmail.com/accountInfo.txt"));
@@ -88,11 +88,11 @@ public class FileOutstreamTest {
     }
 
     @Test
-    public void testSaveNewAccount_Question1_Correct(){
+    public void testSaveNewAccount_Question1_Correct() {
         fileOutstream = new FileOutstream();
         String[] sqa1 = {"1", "MOM"};
         String[] sqa2 = {"5", "HTX"};
-        fileOutstream.saveNewAccount("testEmail@hotmail.com", "testPswd123+","testName","0",sqa1,sqa2);
+        fileOutstream.saveNewAccount("testEmail@hotmail.com", "testPswd123+", "testName", "0", sqa1, sqa2);
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader("Accounts/testEmail@hotmail.com/accountInfo.txt"));
@@ -107,11 +107,11 @@ public class FileOutstreamTest {
     }
 
     @Test
-    public void testSaveNewAccount_Answer1_Correct(){
+    public void testSaveNewAccount_Answer1_Correct() {
         fileOutstream = new FileOutstream();
         String[] sqa1 = {"1", "MOM"};
         String[] sqa2 = {"5", "HTX"};
-        fileOutstream.saveNewAccount("testEmail@hotmail.com", "testPswd123+","testName","0",sqa1,sqa2);
+        fileOutstream.saveNewAccount("testEmail@hotmail.com", "testPswd123+", "testName", "0", sqa1, sqa2);
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader("Accounts/testEmail@hotmail.com/accountInfo.txt"));
@@ -127,11 +127,11 @@ public class FileOutstreamTest {
     }
 
     @Test
-    public void testSaveNewAccount_Question2_Correct(){
+    public void testSaveNewAccount_Question2_Correct() {
         fileOutstream = new FileOutstream();
         String[] sqa1 = {"1", "MOM"};
         String[] sqa2 = {"5", "HTX"};
-        fileOutstream.saveNewAccount("testEmail@hotmail.com", "testPswd123+","testName","0",sqa1,sqa2);
+        fileOutstream.saveNewAccount("testEmail@hotmail.com", "testPswd123+", "testName", "0", sqa1, sqa2);
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader("Accounts/testEmail@hotmail.com/accountInfo.txt"));
@@ -148,11 +148,11 @@ public class FileOutstreamTest {
     }
 
     @Test
-    public void testSaveNewAccount_Answer2_Correct(){
+    public void testSaveNewAccount_Answer2_Correct() {
         fileOutstream = new FileOutstream();
         String[] sqa1 = {"1", "MOM"};
         String[] sqa2 = {"5", "HTX"};
-        fileOutstream.saveNewAccount("testEmail@hotmail.com", "testPswd123+","testName","0",sqa1,sqa2);
+        fileOutstream.saveNewAccount("testEmail@hotmail.com", "testPswd123+", "testName", "0", sqa1, sqa2);
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader("Accounts/testEmail@hotmail.com/accountInfo.txt"));
@@ -170,10 +170,23 @@ public class FileOutstreamTest {
     }
 
     @Test
-    public void testUpdateName_SUCCESS(){
+    public void testUpdateName_SUCCESS() {
+        fileOutstream = new FileOutstream();
+
         try {
             BufferedReader reader = new BufferedReader(new FileReader("Accounts/testEmail@hotmail.com/accountInfo.txt"));
             reader.readLine(); //password
+            String oldName = reader.readLine();
+            reader.close();
 
+            fileOutstream.updateName("testEmail@hotmail.com", oldName, "updatedTestName");
+
+            BufferedReader reader2 = new BufferedReader(new FileReader("Accounts/testEmail@hotmail.com/accountInfo.txt"));
+            reader2.readLine(); //password
+            assertEquals("updatedTestName", reader2.readLine());
+            reader2.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
